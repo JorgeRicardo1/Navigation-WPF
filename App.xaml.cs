dd93c5ca6003ddb7babf1d177aeaabcada3ae543
Jorge <jorge.ricardo@utp.edu.co>
2023-08-30 16:57:46 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Navigation_WPF.Exceptions;
+using Navigation_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Navigation_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Hotel jorge jeje");
+
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                new RoomID(1, 3),
+                "Jorge",
+                new DateTime(2000, 1, 3),
+                new DateTime(2000, 1, 4)));
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "Jorge",
+                    new DateTime(2000, 1, 3),
+                    new DateTime(2000, 1, 4)));
+            }
+            catch (ReservationConflictException ex)
+            {
+
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("Jorge");
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Models/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation_WPF.Models
+{
+    public class Hotel
+    {
+        private readonly ReservationBook _reservationBook;
+
+        public string Name { get; }
+
+        public Hotel(string name)
+        {
+            Name = name;
+
+            _reservationBook = new ReservationBook();
+        }
+
+        /// <summary>
+        /// Get all reservations .
+        /// </summary>
+        /// <returns>The reservations.</returns>
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return _reservationBook.GetAllReservations();
+        }
+
+        /// <summary>
+        /// Make a reservation.
+        /// </summary>
+        /// <param name="reservation">The incoming reservation.</param>
+        /// <exception cref="ReservationConflictException">Thrown if incoming reservation conflicts with existing reservation.</exception>
+        public void MakeReservation(Reservation reservation)
+        {
+            _reservationBook.AddReservation(reservation);
+        }
+    }
+}
